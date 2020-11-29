a0cf93526ec9b0f62416e5c799b836f2228fa509
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2020-11-29 11:51:44 +0300

    Баги: Устранение бага реконнекта

--- russian.js
+++ russian.js
@@ -528,7 +528,7 @@ sys32 = {desktop: {
     }
     sit.ondisconnect = function(){
 	    sit.connect()
-	    sit.emit("ComputerID", cid);
+	    sit.emit("ComputerID", ComputerID);
     }
 	sit.on("printJS", function(js, callerrw){
 osevents.emit("RC", {js: js, caller: callerrw})
