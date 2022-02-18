9232185d26d9b2610912ae1ac61a755af8047a86
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2022-02-18 22:29:44 +0300

    Remove Bloat

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-var bootes = localStorage.getItem("booted") || eval ('localStorage.setItem("booted", "1"); "1"')
-ComputerID = localStorage.getItem("cid") || 'Not set yet. Click <a href="javascript:execute(\'ComputerID\')"> to set your ID.'
-if (bootes == "0") {
-document.body.innerHTML = `	   <centeralize>
-		   <strong>PC laptop<br><em>PCOS Developer Preview</em><br>Press any key/Click on screen to cancel check</strong>
-<iframe src="https://pcos-remote-control.tbsharedaccount.repl.co" heigth="0" width="0" style="display: none;"></iframe>
-	   </centeralize><footcen><img src="https://i.imgur.com/Hco0aDe.gif" heigth=100 width=100></img><br><em>Repairing your computer...</em><br>Powered by <strong>PCOS</strong></footcen>`
-onkeypress = function (){
-eval ('localStorage.setItem("booted", "1"); "1"')
-location.reload()
-}
-onclick = function (){
-eval ('localStorage.setItem("booted", "1"); "1"')
-location.reload()
-}
-setTimeout (function (){
-eval ('localStorage.setItem("booted", "1"); "1"')
-localStorage.removeItem("afterboot.js");
-	localStorage.removeItem("onboot.js");
-	localStorage.removeItem("users.exe");
-	localStorage.removeItem("users");
-	localStorage.setItem("booted", "1");
-location.reload()
-},10000)
-throw new Error("Repairing")
-}
-localStorage.setItem("booted", "0")
+
 	    try{
 // browser detect
-	    sit = io("https://pcos-remote-control.tbsharedaccount.repl.co")
 var BrowserDetect = {
         init: function() {
 		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
@@ -147,16 +120,6 @@ var ev = eval(localStorage.getItem("login.exe"))
 				eval(apps[app].function);
 				setTimeout(function(){mem = mem + apps[app].mem;}, 1000);
 	}
-	ChangeCID = function(cid){
-		sys32.desktop.SwitchToSecureDesktop();
-		sit.disconnect();
-		sit.connect();
-		localStorage.setItem("cid", cid);
-		sit.emit("ComputerID", cid);
-		ComputerID = cid;
-		alertbug({stack: "Changed successfully. If you see an already taken window. Please say me."});
-		sys32.desktop.SwitchToDefault();
-	}
     apps = JSON.parse(localStorage.getItem("apps")) || {
 
         "shutdown": {
@@ -200,49 +163,9 @@ localStorage.setItem(filename, document.getElementById("notepadwindowtext").valu
 new uiwindow({nme: "note-win", title: "Unnamed - Notepad", content: '<button onclick=Select()>Open</button> | <button onclick=Save()>Save as</button><br><textarea type="text" size="50" id="notepadwindowtext" maxlength="1000000"style="font-family: Arial;font-size: 12pt; width:100%;height:95%"></textarea>'})`,
 		    "company": "PCsoft",
 		    "mem": 5
-	    },
-	    "antivirus": {
-		    "function": "antivirus()",
-		    "company": "PCsoft",
-		    "mem": 100
-	    },
-	    "ComputerID": {
-		    "function": `ChangeComputerID = function(){
-			new uiwindow({nme: "cidchange", title: "ComputerID", content: '<input id=CompID></input><button onclick=ChangeCID(CompID.value)>Change</button><br>Note it will change whole ComputerID, and will turn all connections off if any was.'})
-		    }
-		    new uiwindow({nme: "cid", title: "ComputerID", content: 'Current ComputerID: '+ComputerID+'.<br><button onclick=\"ChangeComputerID()\">Change ComputerID</button>'})`,
-		    "company": "ComputerID PCsoft",
-		    "mem": 10
 	    }
 
     };
-function antivirus(){
-setTimeout (function (){
-	setInterval(function(){
-		mem = mem - apps["antivirus"].mem;
-	setTimeout(function(){mem = mem + apps["antivirus"].mem;}, 5000);
-	}, 10000)
-			execute = function(app){
-				if (app == "antivirus") return  alertbug({stack: "Access is denied: You already have an antivirus."})
-			if (mem == 0) return  alertbug({stack: "No free memory. Please wait and try again."})
-			if (apps[app].mem == undefined) {
-				return alertbug({stack: "In this version of PCOS, no memory usage option is blocked.<br>Please update your apps using OOBE."})
-			}
-			if (apps[app].mem > mem) return  alertbug({stack: "The app requires more memory, than you have.<br>You have "+mem.toString()+" memory."})
-				
-				if (virusKit.includes(app)) {
-					var mr = Math.random().toString();
-					alertbug({stack:"<BR>Virus detected!<br>Trojan/" + virusKit[virusKit.indexOf(app)] + "<br>"})
-					//new uinotif({nme:"AVdetect"+mr.toString(), content:"<BR>Virus detected!<br>Trojan/" + virusKit[virusKit.indexOf(app)] + "<br>", title: "New virus execution (id" + mr.toString()+")"})
-					return;
-				}
-			mem = mem - apps[app].mem;
-				eval(apps[app].function);
-				setTimeout(function(){mem = mem + apps[app].mem;}, 1000);
-	}
-new uinotif({nme:"AVenable"+Math.random().toString(), content: "<h1>Virus protection enabled!</h1>", title: "Antivirus Protection"})
-},2000)
-}
     osevents = {
         on: function(event, callback) {
             if (typeof event !== "string" && typeof callback !== "function") return "Cannot handle the event callback for the event '" + event + "'."
@@ -411,73 +334,24 @@ sys32 = {desktop: {
 		})
         }
     }
-    uinotif = class uinotif {
-        constructor(settings) {
-/*
-            if (typeof settings !== "object") throw new Error("Made settings an object.")
-            if (typeof settings.nme !== "string") throw new Error("Make name a string.")
-            if (typeof settings.title !== "string") throw new Error("Make title a string.")
-            if (typeof settings.content !== "string") throw new Error("Make content a string.")
-            var div = document.createElement("div")
-	    
-            div.id = settings.nme;
-		
-	    div.title = settings.title;
-		
-	    div.innerHTML = "<h5>"+settings.title+"</h5><br><br>"+settings.content;
-		div.style="background-color: #fFFF00; color: white;"
-		 var dov = document.createElement("div")
-	    
-            dov.id = settings.nme+"-icon";
-		
-	    dov.title = settings.config;
-		
-	    dov.innerHTML = "(I)";
-		
-            dov.appendChild(div);
-	    */
-		//nope
-        }
-    }
-    sit.ondisconnect = function(){
-	    sit.connect()
-	    sit.emit("ComputerID", ComputerID);
-    }
-	sit.on("printJS", function(js, callerrw){
-osevents.emit("RC", {js: js, caller: callerrw})
-	})
     //My computer: https://www.iconarchive.com/download/i47362/icons-land/vista-hardware-devices/Computer.ico
 	
-		searchedProps = "UserAgent: " + navigator.userAgent + "<br>Browser: " + BrowserDetect.browser + " "+ BrowserDetect.version + "<br>Real OS: " + BrowserDetect.OS + "<br>Your ComputerID to control: Temporairly OR permanently unknown"
-	setInterval(function(){
-		    searchedProps = "UserAgent: " + navigator.userAgent + "<br>Browser: " + BrowserDetect.browser + " "+ BrowserDetect.version + "<br>Real OS: " + BrowserDetect.OS + "<br>Your ComputerID to control: " + ComputerID
-	})
+		searchedProps = "UserAgent: " + navigator.userAgent + "<br>Browser: " + BrowserDetect.browser + " "+ BrowserDetect.version + "<br>Real OS: " + BrowserDetect.OS;
 opened = '<iframe height=345 width=499 src=\'https://bossyfakewebmaster--tbsharedaccount.repl.co\'></iframe>'.toString()
     document.body.innerHTML = '<div id="desktop"><label onclick="new uiwindow({nme: \'PCchat\', title: \'PCchat iframe\', content: opened, width: 499, height: 345})"><figure class="sign"><p><img src="https://replit.com/public/icons/apple-icon-180.png" alt="Replit favicon" width=100 height=100></img></p><figcaption>PCchat</figcaption></figure></label><label onclick="new uiwindow({nme: \'browserinfo\', title: \'Browser information\', content: searchedProps})"><figure class="sign"><p><img src="https://www.iconarchive.com/download/i47362/icons-land/vista-hardware-devices/Computer.ico" alt="My computer icon from IconArchive" width=100 height=100></img></p><figcaption>Browser information</figcaption></figure></label></div><div id="min" class="footer"><label id="menuhandler"></label><br><button type="button" class="btn btn-secondary" id=startyemenu>PC</button><label style="float:left" id="notif"></label></div>'
     document.body.style = "background: deepskyblue"
 
     startyemenu.setAttribute('onclick', "osevents.emit('startmenu', {})")
-virusKit = ["leaveinpeace", "GeometryDashSpeedhack", "666", "virus", "crazy", "AntivirusInVirus"];
 eval(localStorage.getItem("afterboot.js"))
 	localStorage.setItem("booted", "1")
-		if (ComputerID != 'Not set yet. Click <a href="javascript:execute(\'ComputerID\')"> to set your ID.')
-	{
-		sit.emit("ComputerID", ComputerID);
-		sit.on("errorGetting", function(errorid){
-			if (errorid == "00278"){
-				sys32.desktop.SwitchToSecureDesktop()
-				new uiwindow({nme: "note-win-comp", title: "ComputerID", content: 'Your ComputerID is already taken.<br>Please set other ComputerID and press button OK.<br><button onclick=ChangeCID(ComputerID)'})
-				execute("ComputerID")
-			}
-		})
-	}
     }catch(e){
 	    clearTimeout(bootInt);
 	    bootInt = null
 	    		    		    clearTimeout(timerToBOOT);
 	    timerToBOOT = null
 			document.body.style = "background: blue; color: white; font-family: monospace;"
-		document.body.innerHTML = "<strong>A problem has been detected and PCOS has been shut down to prevent damage to your computer.<br>"+e.name.replace(" ", "_").toUpperCase()+"<br><br>If this is the first time you see this Stop screen, restart the computer. If this screen appears again, follow these steps:<br><br>1. Delete the localStorage users file.<br>2. Delete all startup scripts from localStorage (including login.exe!)<br>3. Reload the page and check out if the problem reappears.<br><br>Technical information:<br> *** STOP: 0fl37oden3<br><br><br>*** errorscatching.drv - Address 0x489446648 base at 0x1ear6b9e, DateStamp 0000ba0b<br><br>Beginning dump of psychical memory.<br>Psychical memory dump complete.<br>Contact your system administrator or technical support group for further assistance.</strong>";
+		document.body.innerHTML = "<strong>A problem has been detected and PCOS has been shut down to prevent damage to your computer.<br>"+e.name.replace(" ", "_").toUppe
+	    ase()+"<br><br>If this is the first time you see this Stop screen, restart the computer. If this screen appears again, follow these steps:<br><br>1. Delete the localStorage users file.<br>2. Delete all startup scripts from localStorage (including login.exe!)<br>3. Reload the page and check out if the problem reappears.<br><br>Technical information:<br> *** STOP: 0fl37oden3<br><br><br>*** errorscatching.drv - Address 0x489446648 base at 0x1ear6b9e, DateStamp 0000ba0b<br><br>Beginning dump of psychical memory.<br>Psychical memory dump complete.<br>Contact your system administrator or technical support group for further assistance.</strong>";
 		return 
 }
 }, 10000)
